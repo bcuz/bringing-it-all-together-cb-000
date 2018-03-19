8930e3daf8a07e453a9e39d8928d2c1c7c5850f4
adam n <adam.nawroc@gmail.com>
2018-03-19 11:18:54 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -9,5 +9,15 @@ class Dog
     @breed = breed
   end
 
-  
+  def self.create_table
+    sql = <<-SQL
+    CREATE TABLE IF NOT EXISTS students (
+      id INTEGER PRIMARY KEY,
+      name TEXT,
+      grade TEXT
+    )
+    SQL
+
+    DB[:conn].execute(sql)
+  end
 end
